0694153152e4f149d9d268bdb1c2ae34ae150b17
Arturo Valdez <santiguardado27@gmail.com>
2025-01-29 19:08:55 -0600

    Documentacion de Proyecto

--- src/main.java
+++ src/main.java
@@ -3,8 +3,10 @@ import ventas.Producto;
 
 public class main {
     public static void main(String[] args) {
+        //Titulo
         System.out.println("*** Punto de Venta ***");
 
+        //Productos creados
         var producto1 = new Producto("Blusa", 140.12);
         var producto2 = new Producto("Pantalon", 299.12);
         var producto3 = new Producto("Gorra", 50.00);
@@ -13,11 +15,16 @@ public class main {
         var producto6 = new Producto("Lentes", 230.40);
         var producto7 = new Producto("Guantes", 23.00);
 
+        //Orden de compra
         var orden1 = new OrdenCompra();
+
+        //Agregar productos
         orden1.agregarProducto(producto1);
         orden1.agregarProducto(producto3);
         orden1.agregarProducto(producto7);
         orden1.agregarProducto(producto5);
+
+        //Mostrar orden de compra
         System.out.println(orden1);
 
 
--- src/ventas/OrdenCompra.java
+++ src/ventas/OrdenCompra.java
@@ -1,19 +1,22 @@
 package ventas;
 
-import java.util.Arrays;
-
+//Clase orden de compra
 public class OrdenCompra {
+    //Atributos
     private final int idOrden;
     private Producto[] productos;
     private int contarProducto;
     private static final int MAX_PRODUCTOS =10;
     private static int contadorOrden;
 
+    //Constructor con array de Productos y id Orden
     public OrdenCompra() {
         this.productos = new Producto[OrdenCompra.MAX_PRODUCTOS];
         this.idOrden = ++OrdenCompra.contadorOrden;
     }
 
+    //Metodo vacio donde agrega los productos en memoria y con limite de
+    //Productos almacenados en la Orden de compra
     public void agregarProducto(Producto producto){
         if(this.contarProducto < MAX_PRODUCTOS)
             this.productos[this.contarProducto++] = producto;
@@ -21,6 +24,7 @@ public class OrdenCompra {
             System.out.println("Se ha superado el maximo de Productos: " + MAX_PRODUCTOS);
     }
 
+    //Suma de los precios a los productos seleccionados
     public double calcularTotal(){
         double total = 0;
         for (int i = 0; i < this.contarProducto; i++) {
@@ -30,6 +34,7 @@ public class OrdenCompra {
         return total;
     }
 
+    //Metodo toString para imprecion de Orden de compra
     @Override
     public String toString() {
         var resultado = "Id Orden: " + this.idOrden + "\n";
